b94f8e25ea4e3a3a386552723a3ab0bfc97a4960
Kavitisaikiran <83880759+Kavitisaikiran@users.noreply.github.com>
2023-08-30 20:17:45 +0530

    Update linearSearch.py

--- Array/linearSearch.py
+++ Array/linearSearch.py
@@ -1,1 +1,19 @@
+Here we search for a value by iterating the array  
+Time Complexity is O(n)
+
+General example:
+
+#function definition
+def linear_Search(arr):
+  for i in range(n):
+    if arr[i]== value:
+        return i
+    else:
+        return -1
+
+# calling function
+
+linear_Search(arr)
+arr = []
+value = user_input_number
 
