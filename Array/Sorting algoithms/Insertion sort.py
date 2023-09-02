ef4ab03a79ee9205056f593c2eca4a86949a8ce1
Kavitisaikiran <83880759+Kavitisaikiran@users.noreply.github.com>
2023-09-02 19:30:57 +0530

    Update Insertion sort.py

--- Array/Sorting algoithms/Insertion sort.py
+++ Array/Sorting algoithms/Insertion sort.py
@@ -1,1 +1,14 @@
+Here we create a temp space for the first value and then checks it with every value and increase the second loop 
+Time complexity = O(n^2)
 
+Implementation:
+def insertionSort(arr):
+  for i in range(n):
+    temp = arr[i]
+    for j in range(1,n):
+      j = i-1
+      while (j>=0 and arr[j] > temp):
+        arr[j+1] = arr[j]
+        j = j-1
+      arr[j+1] = temp
+  return arr
