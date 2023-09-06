fe3b73b07d2d0f2fdf95b0115f568932e61983b2
Kavitisaikiran <83880759+Kavitisaikiran@users.noreply.github.com>
2023-09-06 20:17:06 +0530

    Create merge sort.py

---
+++ Array/Sorting algoithms/merge sort.py
@@ -0,0 +1,8 @@
+Merge Sort:
+it is an application of divede and conquer method 
+divide the array based on mid and conquer each by using recursion i.e., calling the same function using mid parameters at upper and lower bound 
+combing the outputs is a process called as Merge Procedure:: it gives a sorted array towards moving up to the root node 
+
+Merge Procedure:: 
+at final root node the left side and right part sub arrays are fully sorted to sort array completely :
+  we take a new array of same size ,the left subarray is intilized with some pointer (i)
