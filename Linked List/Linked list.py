90d53eef88b327575bddf14f8da0df81efd8e525
Kavitisaikiran <83880759+Kavitisaikiran@users.noreply.github.com>
2023-09-09 19:44:36 +0530

    Update Linked list.py

--- Linked List/Linked list.py
+++ Linked List/Linked list.py
@@ -1,1 +1,24 @@
+It is a linear Data Structure
+Dynamic type Data Structure
+ No Continues memory allocation
+No random access is allowed 
+Here we have Node
+Node: 
+    1.) We have Data 
+    2.) Pointer which points to the address of another element // Node
+Hear Node: It refers to the address of the first element node
 
+We always do linear Searches on the Linked list to search for an element coz we can't randomly access elements Time Complexity === O(n)
+Insertion and Deletion are best done in the linked list 
+
+Types of Linked Lists:
+    1.) Singular Linked list
+    2.)Doubly Linked list
+    3.) Circular Linked list
+
+Doubly Linked list:
+      Here we have 2 pointers one stores the address of the next node and other stores the address of previous node
+      last node of the last element is always none 
+Circular Linked list:
+      Here is the doubly linked list concept is applied but there is change
+      the last element next has the address of the first node and similarly the prev has the address of the last node in linked list 
